55a3196350bdaf98c5a22e88b9f1a7992301d0e8
IvoryDuke <iv0ryduk3@gmail.com>
2020-09-07 20:41:39 +0200

    Better naming

--- include/SSVMenuSystem/Items/BindControl.hpp
+++ include/SSVMenuSystem/Items/BindControl.hpp
@@ -26,27 +26,27 @@ namespace ssvms
             JoystickBind
         };
 
-		class BindControl final : public ItemBase
-		{
-		private:
-			using Combo = ssvs::Input::Combo;
-			using KKey = ssvs::KKey;
-			using MBtn = ssvs::MBtn;
+        class BindControl final : public ItemBase
+        {
+        private:
+            using Combo = ssvs::Input::Combo;
+            using KKey = ssvs::KKey;
+            using MBtn = ssvs::MBtn;
             using Trigger = ssvs::Input::Trigger;
-			using TriggerGetter = std::function<Trigger()>;
+            using TriggerGetter = std::function<Trigger()>;
             using SizeGetter = std::function<int()>;
             using BindReturn = std::function<std::pair<int, Trigger>(KKey, MBtn)>;
-            using CallBack = std::function<void(const Trigger&, const int)>;
-            
+            using Callback = std::function<void(const Trigger&, const int)>;
+
             TriggerGetter triggerGetter;
             SizeGetter sizeGetter;
             BindReturn addBind;
-			Action clearBind;
-            CallBack callBack;
+            Action clearBind;
+            Callback callback;
             int triggerID;
-            
+
             bool waitingForBind{false};
-            
+
             [[nodiscard]] inline int getRealSize(const std::vector<Combo>& combos) const
             {
                 int i, size = combos.size();
@@ -55,20 +55,20 @@ namespace ssvms
                 return i;
             }
 
-		public:
-			template <typename TFuncGet, typename TFuncSet, typename TFuncClear, typename TFuncCallBack>
-			BindControl(Menu& mMenu, Category& mCategory, const std::string& mName,
-						TFuncGet mFuncGet, TFuncSet mFuncSet, TFuncClear mFuncClear,
-                        TFuncCallBack mCallBack, int mTriggerID)
-				: ItemBase{mMenu, mCategory, mName},
+        public:
+            template <typename TFuncGet, typename TFuncSet, typename TFuncClear, typename TFuncCallback>
+            BindControl(Menu& mMenu, Category& mCategory, const std::string& mName,
+                        TFuncGet mFuncGet, TFuncSet mFuncSet, TFuncClear mFuncClear,
+                        TFuncCallback mCallback, int mTriggerID)
+                : ItemBase{mMenu, mCategory, mName},
                   triggerGetter{[=, this] { return mFuncGet(); }},
                   sizeGetter{[=, this] { return getRealSize(triggerGetter().getCombos()); }},
-				  addBind{[=, this](const KKey setKey, const MBtn setBtn) { return mFuncSet(setKey, setBtn, sizeGetter()); }},
+                  addBind{[=, this](const KKey setKey, const MBtn setBtn) { return mFuncSet(setKey, setBtn, sizeGetter()); }},
                   clearBind{[=, this] { mFuncClear(sizeGetter()); }},
-                  callBack{mCallBack}, triggerID{mTriggerID}
-			{
-			}
-            
+                  callback{mCallback}, triggerID{mTriggerID}
+            {
+            }
+
             inline void exec() override //if bind spots are full do not allow binding
             {
                 if(!waitingForBind)
@@ -81,13 +81,13 @@ namespace ssvms
                 }
             }
             inline int isWaitingForBind() override { return waitingForBind ? KeyboardBind : 0; }
-			inline bool erase() override
+            inline bool erase() override
             {
                 const int size = sizeGetter();
                 if(!size) { return false; }
-                
+
                 clearBind();
-                callBack(triggerGetter(), triggerID);
+                callback(triggerGetter(), triggerID);
                 return true;
             }
 
@@ -124,37 +124,37 @@ namespace ssvms
 
                 // key was assigned to another function and was unbound.
                 // This trigger must be refreshed as well
-                if(unboundID > -1) { callBack(trig, unboundID); }
+                if(unboundID > -1) { callback(trig, unboundID); }
 
                 // apply the new bind in game
-                callBack(triggerGetter(), triggerID);
+                callback(triggerGetter(), triggerID);
 
                 // finalize
                 waitingForBind = false;
             }
 
-			inline std::string getName() const override
-			{
-				const std::vector<Combo>& combos = triggerGetter().getCombos();
+            inline std::string getName() const override
+            {
+                const std::vector<Combo>& combos = triggerGetter().getCombos();
                 const int size = sizeGetter();
-				std::string bindNames;
+                std::string bindNames;
                 std::bitset<102> keyBind;
                 std::bitset<6> btnBinds;
-				int i, j;
+                int i, j;
 
                 //get binds in the order they have been entered
-				for(i = 0; i < size; ++i)
+                for(i = 0; i < size; ++i)
                 {
                     keyBind = combos[i].getKeys();
-					for(j = 0; j <= KKey::KeyCount; ++j)
-					{
-						if(keyBind[j])
+                    for(j = 0; j <= KKey::KeyCount; ++j)
+                    {
+                        if(keyBind[j])
                         {
                             if(!bindNames.empty()) { bindNames += ", "; }
                             bindNames += ssvs::getKKeyName(KKey(j - 1)); //names are shifted compared to the Key enum
                             break;
                         }
-					}
+                    }
 
                     btnBinds = combos[i].getBtns();
                     for(j = 0; j <= MBtn::ButtonCount; ++j)
@@ -166,41 +166,41 @@ namespace ssvms
                             break;
                         }
                     }
-				}
+                }
 
                 if(waitingForBind) { bindNames += "_"; }
-				
-				return name + ": " + bindNames;
-			}
-		};
+
+                return name + ": " + bindNames;
+            }
+        };
 
         class JoystickBindControl final : public ItemBase
         {
         private:
             using ValueGetter = std::function<int()>;
             using ValueSetter = std::function<int(const int)>;
-            using Bind = std::function<void(const int, const int)>;
+            using Callback = std::function<void(const unsigned int , const int)>;
 
             ValueGetter valueGetter;
             ValueSetter setButton;
-            Bind bind;
+            Callback callback;
             int buttonID;
 
             bool waitingForBind{false};
-			
-			const std::string buttonsNames[12][2] = {
-				{ "A", "SQUARE" }, 			{ "B", "CROSS" }, 				{ "X", "CIRCLE" }, 		{ "Y", "TRIANGLE" },
-				{ "LB", "L1" }, 			{ "RB", "R1" }, 				{ "BACK", "L2" }, 		{ "START", "R2" },
-				{ "LEFT STICK", "SELECT" }, { "RIGHT STICK", "START" }, 	{ "LT", "LEFT STICK" }, { "RT", "RIGHT STICK" } };
+
+            const std::string buttonsNames[12][2] = {
+                { "A", "SQUARE" }, 			{ "B", "CROSS" }, 				{ "X", "CIRCLE" }, 		{ "Y", "TRIANGLE" },
+                { "LB", "L1" }, 			{ "RB", "R1" }, 				{ "BACK", "L2" }, 		{ "START", "R2" },
+                { "LEFT STICK", "SELECT" }, { "RIGHT STICK", "START" }, 	{ "LT", "LEFT STICK" }, { "RT", "RIGHT STICK" } };
 
         public:
-            template <typename TFuncGet, typename TFuncSet, typename TFuncBind>
+            template <typename TFuncGet, typename TFuncSet, typename TFuncCallback>
             JoystickBindControl(Menu& mMenu, Category& mCategory, const std::string& mName,
-                        TFuncGet mFuncGet, TFuncSet mFuncSet, TFuncBind mFuncBind, int mButtonID)
-                : ItemBase{mMenu, mCategory, mName},
-                  valueGetter{[=] { return mFuncGet(); }},
-                  setButton{[=, this](const int pressedButton) { return mFuncSet(pressedButton); }},
-                  bind{mFuncBind}, buttonID{mButtonID}
+                                TFuncGet mFuncGet, TFuncSet mFuncSet, TFuncCallback mCallback, int mButtonID)
+                                : ItemBase{mMenu, mCategory, mName},
+                                valueGetter{[=] { return mFuncGet(); }},
+                                setButton{[=, this](const int pressedButton) { return mFuncSet(pressedButton); }},
+                                callback{mCallback}, buttonID{mButtonID}
             {
             }
 
@@ -215,7 +215,7 @@ namespace ssvms
 
                 // clear both the config and the in game input
                 setButton(33);
-                bind(33, buttonID);
+                callback(33, buttonID);
                 return true;
             }
 
@@ -233,10 +233,10 @@ namespace ssvms
 
                 // if the key was bound to another function and it was reassigned
                 // make sure we also update the unbound joystick button
-                if(unboundID > -1) { bind(33, unboundID); }
+                if(unboundID > -1) { callback(33, unboundID); }
 
                 // update the bind we customized
-                bind(joy, buttonID);
+                callback(joy, buttonID);
 
                 // finalize
                 waitingForBind = false;
@@ -246,27 +246,27 @@ namespace ssvms
             {
                 std::string bindNames;
                 unsigned int value = valueGetter();
-				
-				if(value == 33)
+
+                if(value == 33)
                 {
                     bindNames = "";
                 }
-				else
-				{
-					unsigned int vendorId = sf::Joystick::isConnected(0) ? sf::Joystick::getIdentification(0).vendorId : 0;
-					switch(vendorId)
-					{
-					case MS_VENDOR_ID:
-						bindNames = value >= 12 ? "" : buttonsNames[value][0];
-						break;
-					case SONY_VENDOR_ID:
-						bindNames = value >= 12 ? "" : buttonsNames[value][1];
-						break;
-					default:
-						bindNames = ssvu::toStr(value);
-						break;
-					}
-				}
+                else
+                {
+                    unsigned int vendorId = sf::Joystick::isConnected(0) ? sf::Joystick::getIdentification(0).vendorId : 0;
+                    switch(vendorId)
+                    {
+                        case MS_VENDOR_ID:
+                            bindNames = value >= 12 ? "" : buttonsNames[value][0];
+                            break;
+                        case SONY_VENDOR_ID:
+                            bindNames = value >= 12 ? "" : buttonsNames[value][1];
+                            break;
+                        default:
+                            bindNames = ssvu::toStr(value);
+                            break;
+                    }
+                }
 
                 if(waitingForBind) { bindNames += "_"; }
 
